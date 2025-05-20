1569dfec65af4b14d3dbc3329fe48e7934fc0bb0
acz0077 <anisa_cz220477@yudharta.ac.id>
2025-05-20 19:19:46 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,191 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+const String supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2cmNqdXBha3R4Z2dwaGdvamNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NDIzNDUsImV4cCI6MjA2MzIxODM0NX0.Xj-Ybsgjb528otQ_29FumFzLgh7T1ufs67JZ-5SKBuE';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://bvrcjupaktxggphgojcd.supabase.co',
+    anonKey: supabaseKey,
+  );
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Aplikasi Catatan',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
+        home: MyHomePage(),
       ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void toggleFavorite() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toogleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    return LayoutBuilder(builder: (contex, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: false,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            pair.asLowerCase,
+            style: style,
+            semanticsLabel: "${pair.first} ${pair.second}",
+          )),
     );
   }
 }
---
+++ lib/pagesnote/albumpagenote.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http show get;
+
+// Future<http.Response> fetchAlbum() async {
+// final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+// }
+
+Future<List<Album>> fetchAlbums() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    final List<dynamic> jsonResponse = json.decode(response.body);
+    return jsonResponse.map((album) => Album.fromJson(album)).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load albums');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({required this.userId, required this.id, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    if (json.containsKey('userId') &&
+        json.containsKey('id') &&
+        json.containsKey('title')) {
+      return Album(
+        userId: json['userId'],
+        id: json['id'],
+        title: json['title'],
+      );
+    } else {
+      throw const FormatException('Failed to load album');
+    }
+  }
+}
+
+class AlbumPageNote extends StatefulWidget {
+  const AlbumPageNote({super.key});
+
+  @override
+  State<AlbumPageNote> createState() => _AlbumPageNoteState();
+}
+
+class _AlbumPageNoteState extends State<AlbumPageNote> {
+  late Future<List<Album>> futureAlbums;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbums = fetchAlbums();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Album Page Note")),
+      backgroundColor: Colors.deepOrange,
+      body: Center(
+        child: FutureBuilder<List<Album>>(
+          future: futureAlbums,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // Display the titles of all albums in a column
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                    snapshot.data!.map((album) => Text(album.title)).toList(),
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
